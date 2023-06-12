eda47538f3bb26c4ea087768519425f51dbf3804
mungji7 <119998204+mungji7@users.noreply.github.com>
2023-06-12 23:00:28 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+
+        char[] charArr = message.toCharArray();
+        answer = charArr.length * 2;
+        
+        return answer;
+    }
+}
