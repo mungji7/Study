cdf6b08666ad40e47d324879ef94eefa14c86a03
mungji7 <119998204+mungji7@users.noreply.github.com>
2023-10-26 01:24:37 +0900

    [level unrated] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.05 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/unrated/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/unrated/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -3,14 +3,11 @@ class Solution {
         int answer = 0;
         
         char[] chArr = my_string.toCharArray();
-        char[] numArr = {'1','2','3','4','5','6','7','8','9','0'};
         
         for(char ch : chArr) {
-            for(char num : numArr) {
-                if(num == ch) {
-                    answer += Character.getNumericValue(num);
-                }
-            }
+           if(ch>='0' && ch<='9') {
+               answer += Character.getNumericValue(ch);
+           }
         }
         
         return answer;
