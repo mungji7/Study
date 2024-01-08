4fce0d57e1c945cc8b5653abf52da4c80ec92457
mungji7 <119998204+mungji7@users.noreply.github.com>
2024-01-08 23:02:21 +0900

    [level unrated] Title: 개미 군단, Time: 0.01 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        
+        if (hp % 5 == 0) {
+            answer += hp / 5; 
+            return answer;
+        } else {
+            answer += hp / 5;
+            hp = hp % 5;
+        }
+        
+        if (hp % 3 == 0) {
+            answer += hp / 3;
+            return answer;
+        } else {
+            answer += hp / 3;
+            hp = hp % 3;
+        }
+        
+        answer += hp / 1;
+        
+        return answer;
+    }
+}
