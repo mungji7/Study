07d6a60c9b398804c2fc62753243edfc8f73fbe8
mungji7 <119998204+mungji7@users.noreply.github.com>
2024-02-09 19:55:56 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 0.07 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String number) {
+        int sum = 0;
+        
+        for (char c : number.toCharArray()) {
+            sum += Character.getNumericValue(c);
+        }
+        return sum % 9;
+    }
+}
