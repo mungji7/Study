ebb811092b19e8a54b351bc812acb3e10bb90f35
mungji7 <119998204+mungji7@users.noreply.github.com>
2024-02-11 18:06:45 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.11 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        String[] strArr = String.valueOf(n).split("");
+        
+        for (String s : strArr) {
+            answer += Integer.parseInt(s);
+        }
+
+        return answer;
+    }
+}
